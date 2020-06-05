eedc78d7c8c0a12ec7ec8fe63452439d5cf4b531
fun2cod <65659651+fun2cod@users.noreply.github.com>
2020-06-05 18:10:15 +0200

    Add files via upload

---
+++ Driver/Commande.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Driver
+{
+    class Commande : Icommande
+    {
+        private String cmd;
+  
+        public void Exec(string cmd)
+        {
+            String _Cmd = cmd.Substring(1,1);
+            
+
+            if (cmd.StartsWith("P"))
+            {
+                switch (_Cmd)
+                {
+                    case "T":
+                        {
+                            String _msg = cmd.Substring(cmd.IndexOf(":") + 1, cmd.LastIndexOf(":") - cmd.IndexOf(":") - 1);
+                            Console.WriteLine(_msg);
+                            break;
+                        }
+                    case "S":
+                        {
+                            String f = cmd.Substring(cmd.IndexOf(":") + 1, cmd.IndexOf(",") - cmd.IndexOf(":") - 1);
+                            String c = cmd.Substring(cmd.IndexOf(",") + 1, cmd.LastIndexOf(":") - cmd.IndexOf(",") - 1);
+
+                            int _frequency = System.Convert.ToInt32(f);
+                            int _duration = System.Convert.ToInt32(c);
+                            Console.WriteLine("Ack");
+                            Console.WriteLine("");
+                            Console.Beep(_frequency, _duration);
+
+                            break;
+
+
+                        }
+                    default:
+                        {
+                            Console.WriteLine("NAk");
+                            Console.WriteLine("cmd not Found");
+                            break;
+
+                        }
+                }
+            }
+            else
+            {
+                Console.WriteLine("NAk");
+            }
+        }
+    }
+}
---
+++ Driver/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Driver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+
+            Commande c = new Commande();
+            String cmd = Console.ReadLine();
+            c.Exec(cmd);
+       
+
+            }
+
+ 
+        
+    }
+}
